b42b0ec3e2f40bfc6f224a64a57bc3c6c61ce67d
Agathe van Lamsweerde <agathevanlam@gmail.com>
2020-02-27 12:56:38 +0100

    Further work on the reaction and corrections on diffusion

--- grid.hpp
+++ grid.hpp
@@ -125,23 +125,41 @@ namespace pear {
             return node_coord;
         }
 
-        std::vector<d_type> element(int i) const {
-            std::vector<d_type> elem_nodes(3);
+        std::vector<int> elements_for_node(int node) const {
+            std::vector<int> edge_nodes(15);         // Pre-allocation to upper-limit
+
+            int N = 0;                                  // Number of elements found
+            for (int i = 0; i < nb_elements_ ; i++) {   // Loop on all the elements
+
+                if (elements_[3*(i-1)] == node) || (elements_[3*(i-1)+1] == node) || (elements_[3*(i-1)+2] == node){
+                    ++N;                                                            // Update count
+                    if ( N > edge_nodes.size() ) { edge_nodes.resize(N*2); }        // Resize if necessary
+                    edge_nodes[N-1] = i;
+                }
+
+            }
+
+            edge_nodes.resize(N);                   // Final resize
+            return edge_nodes;
+        }
+
+        std::vector<int> element(int i) const {
+            std::vector<int> elem_nodes(3);
             elem_nodes[0] = elements_[3*(i-1)];
             elem_nodes[1] = elements_[3*(i-1)+1];
             elem_nodes[2] = elements_[3*(i-1)+2];
             return elem_nodes;
         }
 
-        std::vector<d_type> outer_edge(int i) const {
-            std::vector<d_type> edge_nodes(2);
+        std::vector<int> outer_edge(int i) const {
+            std::vector<int> edge_nodes(2);
             edge_nodes[0] = outer_edges_[2*(i-1)];
             edge_nodes[1] = outer_edges_[2*(i-1)+1];
             return edge_nodes;
         }
 
-        std::vector<d_type> inner_edge(int i) const {
-            std::vector<d_type> edge_nodes(2);
+        std::vector<int> inner_edge(int i) const {
+            std::vector<int> edge_nodes(2);
             edge_nodes[0] = inner_edges_[2*(i-1)];
             edge_nodes[1] = inner_edges_[2*(i-1)+1];
             return edge_nodes;
