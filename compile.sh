9840c8b97a7c29fd700be96ad1f39e4b8150a6ef
OctaveOliviers <octave.oliviers@gmail.com>
2020-03-29 22:28:41 +0200

    reordered compile.sh file; next check correctness of boundary conditions

--- compile.sh
+++ compile.sh
@@ -15,9 +15,20 @@ echo "starting command line compilation with flags -Wall and -O3"
 #echo " - nlsolver.hpp"
 #g++ -c ../nlsolver.hpp
 echo " --> making executable"
-g++ -Wall -std=c++17 -O3  software/component.hpp software/diffusion.hpp software/grid.hpp software/rdc.hpp software/reaction.hpp software/nlsolver.hpp software/main.cpp
+g++ -Wall -std=c++17 -O3    software/component.hpp \
+                            software/diffusion.hpp \
+                            software/grid.hpp \
+                            software/rdc.hpp \
+                            software/reaction.hpp \
+                            software/nlsolver.hpp \
+                            software/main.cpp
 echo "compilatioin succeeded"
 cp a.out pear_diffusion
 echo "executable named pear_diffusion in main folder"
-rm a.out software/component.hpp.gch software/diffusion.hpp.gch software/grid.hpp.gch software/nlsolver.hpp.gch software/rdc.hpp.gch software/reaction.hpp.gch
+rm a.out    software/component.hpp.gch \
+            software/diffusion.hpp.gch \
+            software/grid.hpp.gch \
+            software/nlsolver.hpp.gch \
+            software/rdc.hpp.gch \
+            software/reaction.hpp.gch
 echo "cleanup done"
